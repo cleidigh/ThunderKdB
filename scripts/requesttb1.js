56379531f9bcb1b81a273d4fcf0171582b2120b3
Christopher Leidigh <cleidigh@Gmail.com>
2019-09-30 16:13:00 -0400

    update other

--- scripts/requesttb1.js
+++ scripts/requesttb1.js
@@ -519,7 +519,7 @@ async function g1() {
 	// extsJson = extArray;
 	// try {
 	let p = [];
-	for (let index = 50; index < 60; index++) {
+	for (let index = 60; index < 69; index++) {
 		console.debug('GetIndex ' + index);
 		p.push(await getAll(extsJson, { start: (0 + index * 20), end: (19 + index * 20) }));
 
