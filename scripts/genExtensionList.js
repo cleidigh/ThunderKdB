bba00a8ee7792e5bbba979b4b0a67d771c6786c9
Christopher Leidigh <cleidigh@Gmail.com>
2019-10-01 00:24:13 -0400

    update

--- scripts/genExtensionList.js
+++ scripts/genExtensionList.js
@@ -62,8 +62,8 @@ function genExtensionListFromFolders() {
 function genExtensionListFromJson(extsJson) {
 
 	// let extsListFile = fs.readFileSync('extension-list-all-templ.md', 'utf8');
-	let extsListFile = fs.readFileSync('extension-list-tb68-templ.md', 'utf8');
-	// let extsListFile = fs.readFileSync('extension-list-tb60-templ.md', 'utf8');
+	// let extsListFile = fs.readFileSync('extension-list-tb68-templ.md', 'utf8');
+	let extsListFile = fs.readFileSync('extension-list-tb60-templ.md', 'utf8');
 	
 	let extRows = "";
 
@@ -87,9 +87,9 @@ function genExtensionListFromJson(extsJson) {
 	});
 	extsListFile = extsListFile.replace('__ext-table-tb68__', extRows);
 	// console.debug(extsListFile );
-	fs.writeFileSync(`${rootDir}/${extGroupAllDir}/${extGroupTB68Dir}/extension-list-tb68.md`, extsListFile);
+	// fs.writeFileSync(`${rootDir}/${extGroupAllDir}/${extGroupTB68Dir}/extension-list-tb68.md`, extsListFile);
 	// fs.writeFileSync(`${rootDir}/${extGroupAllDir}/extension-list-all.md`, extsListFile);
-	// fs.writeFileSync(`${rootDir}/${extGroupAllDir}/${extGroupTB60Dir}/extension-list-tb60.md`, extsListFile);
+	fs.writeFileSync(`${rootDir}/${extGroupAllDir}/${extGroupTB60Dir}/extension-list-tb60.md`, extsListFile);
 
 	console.debug('Done');
 }
@@ -200,13 +200,13 @@ function createExtMDTableRow(extJson) {
 	// }
 
 	// target 60
-	// if ( !(comp_badges.includes(cBadge_tb60)) || comp_badges.includes(cBadge_tb68))  {
-	// 	return "";
-	// }
-
-	if ( !(comp_badges.includes(cBadge_tb68) ) ) {
+	if ( !(comp_badges.includes(cBadge_tb60)) || comp_badges.includes(cBadge_tb68))  {
 		return "";
 	}
+
+	// if ( !(comp_badges.includes(cBadge_tb68) ) ) {
+	// 	return "";
+	// }
 
 	let rank = extJson.xpilib.rank;
 	
