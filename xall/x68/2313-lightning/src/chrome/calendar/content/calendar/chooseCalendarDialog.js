8cc6c076b29b1b1960f903c8f58e42377132be63
Christopher Leidigh <cleidigh@Gmail.com>
2020-01-02 18:36:23 -0500

    update

---
+++ xall/x68/2313-lightning/src/chrome/calendar/content/calendar/chooseCalendarDialog.js
@@ -0,0 +1,91 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* exported loadCalendars */
+
+/* import-globals-from ../calendar-ui-utils.js */
+
+var { cal } = ChromeUtils.import("resource://calendar/modules/calUtils.jsm");
+
+function loadCalendars() {
+    const calendarManager = Cc["@mozilla.org/calendar/manager;1"]
+                              .getService(Ci.calICalendarManager);
+    let listbox = document.getElementById("calendar-list");
+    let composite = cal.view.getCompositeCalendar(window.opener);
+    let selectedIndex = 0;
+    let calendars;
+
+    if (window.arguments[0].calendars) {
+        calendars = window.arguments[0].calendars;
+    } else {
+        calendars = calendarManager.getCalendars({});
+    }
+    calendars = sortCalendarArray(calendars);
+
+    for (let i = 0; i < calendars.length; i++) {
+        let calendar = calendars[i];
+        let listItem = document.createXULElement("richlistitem");
+
+        let colorCell = document.createXULElement("box");
+        try {
+            let calColor = calendar.getProperty("color");
+            colorCell.style.background = calColor || "#a8c2e1";
+        } catch (e) {}
+        colorCell.style.width = "17px";
+        listItem.appendChild(colorCell);
+
+        let nameCell = document.createXULElement("label");
+        nameCell.setAttribute("value", calendar.name);
+        nameCell.setAttribute("flex", "1");
+        listItem.appendChild(nameCell);
+
+        listItem.calendar = calendar;
+        listbox.appendChild(listItem);
+
+        // Select the default calendar of the opening calendar window.
+        if (calendar.id == composite.defaultCalendar.id) {
+            selectedIndex = i;
+        }
+    }
+    document.getElementById("prompt").textContent = window.arguments[0].promptText;
+    if (window.arguments[0].promptNotify) {
+        document.getElementById("promptNotify").textContent = window.arguments[0].promptNotify;
+    }
+
+    // this button is the default action
+    let accept = document.documentElement.getButton("accept");
+    if (window.arguments[0].labelOk) {
+        accept.setAttribute("label", window.arguments[0].labelOk);
+        accept.removeAttribute("hidden");
+    }
+
+    let extra1 = document.documentElement.getButton("extra1");
+    if (window.arguments[0].labelExtra1) {
+        extra1.setAttribute("label", window.arguments[0].labelExtra1);
+        extra1.removeAttribute("hidden");
+    } else {
+        extra1.setAttribute("hidden", "true");
+    }
+
+    if (calendars.length) {
+        listbox.ensureIndexIsVisible(selectedIndex);
+        listbox.timedSelect(listbox.getItemAtIndex(selectedIndex), 0);
+    } else {
+        // If there are no calendars, then disable the accept button
+        document.documentElement.getButton("accept").setAttribute("disabled", "true");
+    }
+
+    window.sizeToContent();
+}
+
+document.addEventListener("dialogaccept", () => {
+    let listbox = document.getElementById("calendar-list");
+    window.arguments[0].onOk(listbox.selectedItem.calendar);
+});
+
+document.addEventListener("dialogextra1", () => {
+    let listbox = document.getElementById("calendar-list");
+    window.arguments[0].onExtra1(listbox.selectedItem.calendar);
+    window.close();
+});
---
+++ xall/x68/2313-lightning/src/modules/utils/calWindowUtils.jsm
@@ -0,0 +1,71 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var { AppConstants } = ChromeUtils.import("resource://gre/modules/AppConstants.jsm");
+var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+/*
+ * Calendar window helpers, e.g. to open our dialogs
+ */
+
+// NOTE: This module should not be loaded directly, it is available when
+// including calUtils.jsm under the cal.window namespace.
+
+this.EXPORTED_SYMBOLS = ["calwindow"]; /* exported calwindow */
+
+var calwindow = {
+    /**
+     * Opens the Create Calendar wizard
+     *
+     * @param aWindow    the window to open the dialog on, or null for the main calendar window
+     * @param aCallback  a function to be performed after calendar creation
+     */
+    openCalendarWizard: function(aWindow, aCallback) {
+        let window = aWindow || calwindow.getCalendarWindow();
+        window.openDialog("chrome://calendar/content/calendarCreation.xul", "caEditServer",
+                          // Workaround for Bug 1151440 - the HTML color picker won't work
+                          // in linux when opened from modal dialog
+                          AppConstants.platform == "linux"
+                              ? "chrome,titlebar,resizable"
+                              : "modal,chrome,titlebar,resizable",
+                          aCallback);
+    },
+
+    /**
+     * Opens the calendar properties window for aCalendar
+     *
+     * @param aWindow    the window to open the dialog on, or null for the main calendar window
+     * @param aCalendar  the calendar whose properties should be displayed
+     */
+    openCalendarProperties: function(aWindow, aCalendar) {
+        let window = aWindow || calwindow.getCalendarWindow();
+        window.openDialog("chrome://calendar/content/calendar-properties-dialog.xul",
+                          "CalendarPropertiesDialog",
+                          // Workaround for Bug 1151440 - the HTML color picker won't work
+                          // in linux when opened from modal dialog
+                          AppConstants.platform == "linux"
+                              ? "chrome,titlebar,resizable"
+                              : "modal,chrome,titlebar,resizable",
+                          { calendar: aCalendar });
+    },
+
+    /**
+     * Opens the print dialog
+     *
+     * @param aWindow    the window to open the dialog on, or null for the main calendar window
+     */
+    openPrintDialog: function(aWindow=null) {
+        let window = aWindow || calwindow.getCalendarWindow();
+        window.openDialog("chrome://calendar/content/calendar-print-dialog.xul", "Print",
+                          "centerscreen,chrome,resizable");
+    },
+
+    /**
+     * Returns the most recent calendar window in an application independent way
+     */
+    getCalendarWindow: function() {
+        return Services.wm.getMostRecentWindow("calendarMainWindow") ||
+               Services.wm.getMostRecentWindow("mail:3pane");
+    }
+};
